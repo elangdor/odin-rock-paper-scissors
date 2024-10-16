a17e5a171ec0799e2a2221234e210e054ac8a4ff
elangdor <179868961+elangdor@users.noreply.github.com>
2024-10-16 22:04:41 +0200

    Added choices for comp & ply added function to get random computers choice added function to get player choice playerchoice handles uppercase/lowercase added logging and errors if anything goes wrong w. choices

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 console.log("tjena tjena");
+let hasPlayerChosen = false;
+let playerChoice;
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()*3)+1;
+    console.log(randomNumber);
+    
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else if (randomNumber === 3) {
+        return "scissors";
+    } else {
+        alert("something went wrong in getComputerChoice!");
+    }
+}
+
+
+    function getPlayerChoice() {
+        let playerChoice = prompt("Choose between rock, paper, scissors:");
+        playerChoice = playerChoice.toLowerCase();
+        if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+            console.log(playerChoice);
+            hasPlayerChosen = true;
+            return playerChoice;
+        } else if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+            alert("You didn't enter a correct choice, try again!");
+            console.log("Wrong player choice! it was: " + playerChoice);
+        } else {
+            alert("something went wrong in getPlayerChoice!");
+        }
+    }
+
+
+let computerChoice = getComputerChoice();
+
+while (hasPlayerChosen === false) {
+    playerChoice = getPlayerChoice();
+}
+
+console.log("The computers choice is: " + computerChoice);
+console.log("The players choice is: " + playerChoice);
+
+
+
